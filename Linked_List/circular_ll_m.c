04934f2dcab994b35e5177b81710c84edd7bb175
Blaze <103813459+sujalshaw1@users.noreply.github.com>
2022-05-14 11:58:24 +0530

    Update circular_ll_m.c

--- Linked_List/circular_ll_m.c
+++ Linked_List/circular_ll_m.c
@@ -3,13 +3,14 @@
 node *create(int data)
 {
     node *temp = (node *)malloc(sizeof(node));
-    temp->next = NULL;  temp->data = data;
+    temp->next = NULL;
+    temp->data = data;
     return temp;
 }
 
 void display(node *head)
 {
-    if(head == NULL)
+    if (head == NULL)
     {
         printf("List is empty.\n");
         return;
@@ -19,8 +20,7 @@ void display(node *head)
     {
         printf("%d --> ", curr->data);
         curr = curr->next;
-    }
-    while(curr != head);
+    } while (curr != head);
     printf("\n");
     return;
 }
@@ -28,43 +28,55 @@ void display(node *head)
 void append(node **head, int data)
 {
     node *new = create(data);
-    if(*head == NULL)
+    if (*head == NULL)
     {
         *head = new;
         new->next = *head;
     }
     node *curr = *head;
-    while(curr->next != *head)
+    while (curr->next != *head)
         curr = curr->next;
     curr->next = new;
     new->next = *head;
 }
 
-void delete(node **head, int value)
+void delete(node **head, int n)
 {
-    node *curr = NULL, *temp = NULL;
-    if((*head)->next == *head && (*head)->data == value)
+    node *curr = *head, *temp;
+    if(*head == NULL){
+        return;
+    }
+    else if(n == 1 && (*head)->next == NULL)
     {
-        curr = *head;
         *head = NULL;
         return;
     }
-    curr = *head;
-    while(curr->data != value)
+    else if(n == 1)
     {
-        if(curr->next == *head)
-            return;
-        curr = curr->next;
+        curr->data = curr->next->data;
+        temp = curr->next;
+        curr->next = curr->next->next;
+        free(temp);
     }
-    curr->data = curr->next->data;
-    temp = curr->next;
-    curr->next = curr->next->next;
-    free(temp);
+    else
+    {
+        int i = 1;
+        for(; i <= n-1; i++)
+        {
+            curr = curr->next;
+            if(curr->next == *head)
+                return;
+        }
+        curr->data = curr->next->data;
+        temp = curr->next;
+        curr->next = curr->next->next;
+        free(temp);
+    }
 }
 
 void reverse(node **head)
 {
-    if(*head == NULL || (*head)->next == *head)
+    if (*head == NULL || (*head)->next == *head)
         return;
     node *curr = *head, *prev = NULL, *next = NULL;
     do
@@ -73,8 +85,8 @@ void reverse(node **head)
         curr->next = prev;
         prev = curr;
         curr = next;
-    }
-    while(next != *head);
+    } while (next != *head);
     curr->next = prev;
     *head = prev;
 }
+
